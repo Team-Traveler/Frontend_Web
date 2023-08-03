f3149ca1a9f43bf12c3ecba73f0ce41ab8df94e4
Dawon00 <dawon000203@gmail.com>
2023-08-03 22:57:29 +0900

    [FE-06] main page 디자인 추가

--- src/recoil/atoms/contentState.js
+++ src/recoil/atoms/contentState.js
@@ -1,68 +1,52 @@
 import { atom } from "recoil";
 
 // 여행 컨텐츠 데이터
-const defaultContents = [
-    {
-        id: 1,
-        title: "Sample Content 1",
-        duration: "3 days 2 nights",
-        description: "This is the description of Sample Content 1.",
-    },
-    {
-        id: 2,
-        title: "Sample Content 2",
-        duration: "2 days 1 night",
-        description: "This is the description of Sample Content 2.",
-    },
-    {
-        id: 3,
-        title: "Sample Content 3",
-        duration: "4 days 3 nights",
-        description: "This is the description of Sample Content 3.",
-    },
-    {
-        id: 4,
-        title: "Sample Content 4",
-        duration: "5 days 4 nights",
-        description: "This is the description of Sample Content 4.",
-    },
-    {
-        id: 5,
-        title: "Sample Content 5",
-        duration: "2 days 1 night",
-        description: "This is the description of Sample Content 5.",
-    },
-    {
-        id: 6,
-        title: "Sample Content 6",
-        duration: "3 days 2 nights",
-        description: "This is the description of Sample Content 6.",
-    },
-    {
-        id: 7,
-        title: "Sample Content 7",
-        duration: "6 days 5 nights",
-        description: "This is the description of Sample Content 7.",
-    },
-    {
-        id: 8,
-        title: "Sample Content 8",
-        duration: "4 days 3 nights",
-        description: "This is the description of Sample Content 8.",
-    },
-    {
-        id: 9,
-        title: "Sample Content 9",
-        duration: "3 days 2 nights",
-        description: "This is the description of Sample Content 9.",
-    },
-    {
-        id: 10,
-        title: "Sample Content 10",
-        duration: "2 days 1 night",
-        description: "This is the description of Sample Content 10.",
-    },
-];
+const defaultContents = {
+    result: [
+        {
+            id: 1,
+            title: "도쿄 여행",
+            duration: "2박 3일",
+            description: "도쿄 디즈니 랜드 정복기", // 15자 이내
+        },
+        {
+            id: 2,
+            title: "대만 여행",
+            duration: "3박 4일",
+            description: "나의 첫 가오슝 자유여행",
+        },
+        {
+            id: 3,
+            title: "싱가폴 여행",
+            duration: "4박 5일",
+            description: "그레이트 세일에 득템하기",
+        },
+        {
+            id: 4,
+            title: "태국 여행",
+            duration: "3박 4일",
+            description: "러이끄라통 등불 축제에 가다",
+        },
+        {
+            id: 5,
+            title: "스페인 여행",
+            duration: "4박 5일",
+            description: "토마토 축제의 로망",
+        },
+        {
+            id: 6,
+            title: "영국 여행",
+            duration: "3박 4일",
+            description: "흐린날씨도 영국의 매력",
+        },
+        {
+            id: 7,
+            title: "헝가리 여행",
+            duration: "3박 4일",
+            description: "아름다운 야경, 저렴한 물가",
+        },
+    ],
+};
 
 export const contentState = atom({
     key: "contentState",
