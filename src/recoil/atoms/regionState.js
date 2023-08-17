32ba334dcdcd2b4dda54bb9502de410fe26879f3
Dawon00 <dawon000203@gmail.com>
2023-08-17 17:17:30 +0900

    [FE-24] 추천폼작성 버그 수정

--- src/recoil/atoms/regionState.js
+++ src/recoil/atoms/regionState.js
@@ -4,17 +4,17 @@ import { atom } from "recoil";
 const defaultContents = {
     result: [
         {
-            id: 1,
+            did: 1,
             city: "속초시",
             country: "강원도",
         },
         {
-            id: 2,
+            did: 2,
             city: "양양군",
             country: "강원도",
         },
         {
-            id: 3,
+            did: 3,
             city: "고성군",
             country: "강원도",
         },
@@ -24,7 +24,7 @@ const defaultContents = {
             country: "강원도",
         },
         {
-            id: 5,
+            did: 5,
             city: "수원시",
             country: "경기도",
         },
@@ -34,37 +34,37 @@ const defaultContents = {
             country: "경기도",
         },
         {
-            id: 7,
+            did: 7,
             city: "성남시",
             country: "경기도",
         },
         {
-            id: 8,
+            did: 8,
             city: "안양시",
             country: "경기도",
         },
         {
-            id: 9,
+            did: 9,
             city: "평택시",
             country: "경기도",
         },
         {
-            id: 10,
+            did: 10,
             city: "부천시",
             country: "경기도",
         },
         {
-            id: 11,
+            did: 11,
             city: "안산시",
             country: "경기도",
         },
         {
-            id: 12,
+            did: 12,
             city: "의정부시",
             country: "경기도",
         },
         {
-            id: 13,
+            did: 13,
             city: "김포시",
             country: "경기도",
         },
