205e036ba0a055060fbd4d3775e52cbb341670ab
iamyeongg <syz101@naver.com>
2023-08-18 15:12:42 +0900

    FE-26

--- src/App.js
+++ src/App.js
@@ -7,34 +7,34 @@ import KakaoLogin from "./pages/MainPage/KakaoLogin";
 import CompletedPage from "./pages/RecommendPage/CompletedPage";
 import { userInfoState } from "./recoil/atoms/userState";
 import { useRecoilState } from "recoil";
+import CommunityPage from "./pages/communityPage/community.js";
+import Search from "./pages/communityPage/search.js";
 
 function App() {
-    const [userInfo] = useRecoilState(userInfoState);
-    const isLogin = userInfo.isLogin;
-    return (
-        <div className="App">
-            <Routes>
-                <Route path="/" element={<MainPage />}></Route>
-                {isLogin ? (
-                    <>
-                        <Route
-                            path="/recommend"
-                            element={<RecommendPage />}
-                        ></Route>
-                        <Route
-                            path="/recommendCompleted"
-                            element={<CompletedPage />}
-                        ></Route>
-                        <Route path="/story" />
-                        <Route path="/note" element={<NotePage />}></Route>
-                        <Route path="/mypage" />
-                    </>
-                ) : null}
-                <Route path="/kakaoLogin" element={<KakaoLogin />} />
-                <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-        </div>
-    );
+  const [userInfo] = useRecoilState(userInfoState);
+  const isLogin = userInfo.isLogin;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<MainPage />}></Route>
+        {isLogin ? (
+          <>
+            <Route path="/recommend" element={<RecommendPage />}></Route>
+            <Route
+              path="/recommendCompleted"
+              element={<CompletedPage />}
+            ></Route>
+            <Route path="/story" element={<CommunityPage />}></Route>
+            <Route path="/story/search" element={<Search />}></Route>
+            <Route path="/note" element={<NotePage />}></Route>
+            <Route path="/mypage"></Route>
+          </>
+        ) : null}
+        <Route path="/kakaoLogin" element={<KakaoLogin />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
