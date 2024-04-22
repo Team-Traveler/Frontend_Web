727520cc00b3a641612a7d6be95876ad8c7c90db
HideOnCodec <tlsdmsgp33@naver.com>
2024-04-22 21:57:59 +0900

    merge with FE-66

--- src/recoil/atoms/userState.js
+++ src/recoil/atoms/userState.js
@@ -3,7 +3,10 @@ import { recoilPersist } from 'recoil-persist';
 import profileTest from "../../assets/images/profileTest.png";
 
 // user 관련 atoms
-const { persistAtom } = recoilPersist();
+const { persistAtom } = recoilPersist({
+    key: "sessionStorage", //원하는 key 값 입력
+    storage: sessionStorage,
+  })
 
 export const userInfoState = atom({
     //유저 정보
